3660a9359d5c4c385b3a20f6b3bacc6dcdb7d24b
Shemistan <shemistan1@gmail.com>
2021-08-09 01:00:22 +0300

    Добавил возможность удаления клона фото

--- script.py
+++ script.py
@@ -4,6 +4,11 @@ from collections import defaultdict
 
 
 class Equalizer:
+    """Script for finding identical photos
+     of various formats with the ability
+     to remove clones"""
+
+
     IMAGE_FORMATS = ('.jpg', '.png', '.JPG', '.PNG', '.bmp', '.BMP')
 
     def __init__(self, path, size=None):
@@ -18,7 +23,7 @@ class Equalizer:
         n = 0
         for dirpath, dirnames, filenames in os.walk(self.path):
             for file in filenames:
-                if filenames != [] and file[-4:] in self.IMAGE_FORMATS:
+                if file != [] and file[-4:] in self.IMAGE_FORMATS:
                     dirnames = os.path.join(dirpath, file)
                     self.dict_all_photos[n] = [dirpath, dirnames, file]
                     n += 1
@@ -26,26 +31,27 @@ class Equalizer:
     def check_for_size(self):
         comparision_dick = dict(self.dict_all_photos)
         new_dict_all_photos = defaultdict(int)
-        ch = len(self.dict_all_photos)
-        for i in self.dict_all_photos.keys():
-            ch -= 1
-            print('Progress of implementation check_by_size ----->  ', ch)
+        execution_process = len(self.dict_all_photos)
+        for key_dict_1 in self.dict_all_photos.keys():
+            execution_process -= 1
+            print('Progress of implementation check_by_size ----->  ', execution_process)
             list_identic_photo = []
-            current_file = os.stat(self.dict_all_photos[i][1]).st_size
-            del comparision_dick[i]
-            for n in comparision_dick.keys():
-                check_file = os.stat(comparision_dick[n][1]).st_size
+            current_file = os.stat(self.dict_all_photos[key_dict_1][1]).st_size
+            del comparision_dick[key_dict_1]
+            for key_dict_2 in comparision_dick.keys():
+                check_file = os.stat(comparision_dick[key_dict_2][1]).st_size
                 if current_file == check_file:
-                    list_identic_photo.append(comparision_dick[n][1])
+                    list_identic_photo.append(comparision_dick[key_dict_2][1])
             if list_identic_photo:
-                new_dict_all_photos[self.dict_all_photos[i][1]] = list_identic_photo
+                key_new_dict_all_photos = self.dict_all_photos[key_dict_1][1]
+                new_dict_all_photos[key_new_dict_all_photos] = list_identic_photo
         self.dict_all_photos = dict(new_dict_all_photos)
 
     def check_for_pixel(self):
-        ch = len(self.dict_all_photos)
+        execution_process = len(self.dict_all_photos)
         for foto, identic_fotos in self.dict_all_photos.items():
-            ch -= 1
-            print('Progress of implementation check_for_pixel ----->  ', ch)
+            execution_process -= 1
+            print('Progress of implementation check_for_pixel ----->  ', execution_process)
             current_file = self.work_with_photos(photo=foto)
             for identic_foto in identic_fotos:
                 check_file = self.work_with_photos(photo=identic_foto)
@@ -75,4 +81,4 @@ photo.looking_for_all_photos()
 photo.check_for_size()
 photo.check_for_pixel()
 photo.show()
-#photo.remove_clones_of_photo()
+# photo.remove_clones_of_photo()
